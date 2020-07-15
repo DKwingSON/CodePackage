28b1dc233343760d12542a56648f6c64fe947734
dkwingson <429482349@qq.com>
2020-07-16 00:03:11 +0800

    优化消息队列

--- MessageQueue.php
+++ MessageQueue.php
@@ -6,6 +6,8 @@ class MessageQueue{
     static public function setTimerId($Id){
         self::$timerId = $timerId;
     }
+
+    //获取对象
     static public function getInstance(){
         if(is_null(self::$oInstance)){
             self::$oInstance = new \Swoole\Corountine\Channel(10);
@@ -13,16 +15,24 @@ class MessageQueue{
         return self::$oInstance;
     }
 
+    //消息队列消耗一个对象;
     protected function consume(){
         if(!self::$oInstance instanceof \Swoole\Corountine\Channel){
             self::$oInstance = new \Swoole\Corountine\Channel(10);
         }
         $sOrder = self::$oInstance->pop();
-        //写DB
-        ;
+        //TODO 解码sOrder
+        $m_oDBProxy = new \DBConn();
+        $m_configDB = $this->m_oDBProxy->getCfg();
+        //TODO 写DB, INSERT
+        $sql = "";
+        $iRet = $m_oDBProxy->oConnDB->exec_update($sql);
         //失败回滚
-        $iRet = $this->product($sOrder);
-        return 0;
+        if($iRet != 0){
+            $iRet = $this->product($sOrder);
+            return $iRet;
+        }
+        else return 0;
     }
 
     protected function product($sOrder){
@@ -35,19 +45,20 @@ class MessageQueue{
 }
 
 //调用方式
-$oMQ = \MessageQueue::getInstance();
 //开一个协程自动消耗
-while(1){
-    //自动阻塞
-    $iRet = $oMQ->consume();
-    switch($iRet){
-        case 0:
-            $sMsg = "Consume Success";
-        break;
-        case 1:
-            $sMsg = "Consume Failed";
-        break;
-        default: $sMsg = "Timeout";
+go(function(){
+    while(1){
+        //自动阻塞
+        $iRet = \MessageQueue::getInstance()->consume();
+        switch($iRet){
+            case 0:
+                $sMsg = "Consume Success";
+            break;
+            case 1:
+                $sMsg = "Consume Failed";
+            break;
+            default: $sMsg = "Timeout";
+        }
+        echo $sMsg.PHP_EOL;
     }
-    echo $sMsg.PHP_EOL;
-}
+});
