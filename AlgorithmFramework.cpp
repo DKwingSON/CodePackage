3b54728549a2a76a9870635446729da9e0bc7ccd
dkwingson <429482349@qq.com>
2020-07-19 21:06:58 +0800

    +并查集

--- AlgorithmFramework.cpp
+++ AlgorithmFramework.cpp
@@ -68,7 +68,68 @@ void dfs()
     }  
 } 
 
+class UF {
+private:
+    // 记录连通分量个数
+    int count;
+    // 存储若干棵树
+    vector<int> parent;
+    // 记录树的“重量”
+    vector<int> size;
+
+    UF(int n) {
+        count = n;
+        parent = vector<int>(n, 0);
+        size = vector<int>(n, 0);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    
+public:
+    /* 将 p 和 q 连通 */
+    void _union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        //判断是不是一个并查集里的
+        if (rootP == rootQ)
+            return;
+        
+        // 小树接到大树下面，较平衡
+        if (size[rootP] > size[rootQ]) {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        } else {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        }
+        count--;
+    }
+
+    /* 判断 p 和 q 是否互相连通 */
+    bool connected(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        // 处于同一棵树上的节点，相互连通
+        return rootP == rootQ;
+    }
+
+    /* 返回节点 x 的根节点 */
+    int find(int x) {
+        while (parent[x] != x) {
+            // 进行路径压缩
+            parent[x] = parent[parent[x]];
+            x = parent[x];
+        }
+        return x;
+    }
+    
+    int count() {
+        return count;
+    }
+};
+
 
 // https://blog.csdn.net/weixin_30299539/article/details/97154546
 // check redis set expire是否设置为同一个原子操作
-//
