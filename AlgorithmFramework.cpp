2430641508c016aab27d1db60286b0839c7809b7
drakeywang <drakeywang@tencent.com>
2020-07-22 21:18:58 +0800

    +生产者消费者框架

--- AlgorithmFramework.cpp
+++ AlgorithmFramework.cpp
@@ -130,6 +130,38 @@ public:
     }
 };
 
+void PVop(){
+    int in=0,out=0;
+    semaphore empty,full,mutex;
+    empty.value=n;    //空缓冲区资源信号量，初始状态有n个空的缓冲区
+    full.value=0;    //满缓冲区资源信号量，初始状态有0个满缓冲区
+    mutex.value=0;    //缓冲区互斥访问量
 
-// https://blog.csdn.net/weixin_30299539/article/details/97154546
-// check redis set expire是否设置为同一个原子操作
+    void producer()    //生产者程序
+    {
+        while(true)
+        {
+            item=producerItem();    //生产一个产品
+            p(empty);    //申请一个空缓冲区。如有，继续执行；否则，此处进程挂起
+            p(mutex);    //如果没有其他进程访问缓冲区，继续执行；否则，进程挂起
+            putItemIntoBuffer(item);    //将产品放入缓冲区
+            in=(in+1)mod n;    //修改指针位置
+            v(mutex);    //解锁，离开缓冲区，如果有其他进程等待进入，则唤醒
+            v(full);    //增加一个满缓冲区，如果有消费者等待，则唤醒
+        }
+    }
+
+    void consumer()    //消费者程序
+    {
+        while(true)
+        {
+            p(full);    //如果有满缓冲区，则继续执行；否则，挂起
+            p(mutex);    //如果没有其他进程访问缓冲区，继续执行；否则，挂起
+            item=removeItemFromBuffer();    //从缓冲区取走一个产品
+            out=(out+1)mod n;    //修改指针位置
+            consume(item);    //消费产品
+            v(mutex);    //解锁，离开缓冲区，如果有其他进程等待进入，则唤醒
+            v(empty);    //增加一个空缓冲区，如果有生产者等待，则唤醒
+        }
+    }
+}
